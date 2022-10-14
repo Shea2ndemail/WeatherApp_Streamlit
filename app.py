b99eb51f9cecd69c94025ca9ba8ccf13ece864dc
Shea2ndemail <50683240+Shea2ndemail@users.noreply.github.com>
2022-10-14 15:52:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ engine = create_engine('postgresql://postgres:12345@192.168.1.201:5432/test')
 
 st.title('Evans Home Weather')
 
-df = pd.read_sql_query('select * from weather_station',con=engine)
+df = pd.read_sql_query('select * from weather_station ORDER BY datetime DESC LIMIT 1',con=engine)
 
 st.write(df)
