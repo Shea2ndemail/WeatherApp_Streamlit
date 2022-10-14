76b710243cc45c4e26a15161cb3c8b6b360f5c4d
Shea2ndemail <50683240+Shea2ndemail@users.noreply.github.com>
2022-10-14 15:46:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+engine = create_engine('postgresql://postgres:12345@192.168.1.201:5432/test')
 
 st.title('Evans Home Weather')
 
+df = pd.read_sql_query('select * from weather_station',con=engine)
+
+st.write(df)
