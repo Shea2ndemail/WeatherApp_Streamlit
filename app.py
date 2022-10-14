76514516ba58645decc184840ea7b6ee46a32a37
Shea2ndemail <shea.n.evans@gmail.com>
2022-10-14 09:55:42 -0500

    testing

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from sqlalchemy import create_engine
 
-st.title('Uber pickups in NYC')
+engine = create_engine('postgresql://postgres:12345@192.168.1.201:5432/test')
 
-st.write("hello")
+st.title('Evans Home Weather')
+
+df = pd.read_sql_query('select * from weather_station',con=engine)
+
+st.write(df)
