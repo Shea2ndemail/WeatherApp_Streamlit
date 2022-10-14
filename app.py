af9f563cdf3c02160bb589add8624a60bd4fb4d9
Shea2ndemail <50683240+Shea2ndemail@users.noreply.github.com>
2022-10-14 16:12:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
 
-#engine = create_engine('postgresql://postgres:12345@{}:5432/test'.format()
+engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(st.secrets["postgres"]["user"], st.secrets["postgres"]["password"], st.secrets["postgres"]["host"], st.secrets["postgres"]["port"], st.secrets["postgres"]["dbname"])
 
 st.title('Evans Home Weather')
 
-#df = pd.read_sql_query('select * from weather_station ORDER BY datetime DESC LIMIT 1',con=engine)
+df = pd.read_sql_query('select * from weather_station ORDER BY datetime DESC LIMIT 1',con=engine)
 
-#st.write(df)
-
-st.write(st.secrets["postgres"])
+st.write(df)
