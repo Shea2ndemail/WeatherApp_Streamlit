2be5507c4c813749279fe1b531be0ed5cc4c9338
Shea2ndemail <50683240+Shea2ndemail@users.noreply.github.com>
2022-10-16 13:02:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import streamlit as st
+from sqlalchemy import create_engine
 import pandas as pd
 
-df = pd.DataFrame({'a':[1,2,3,4], 'b': [23423,343243,34234,44444444444]})
+host = 'ec2-44-209-24-62.compute-1.amazonaws.com'
+db = 'deqji7h232uoc8'
+user = 'lexcwuwkjlbzok'
+port = '5432'
+pwd = 'de7a0a0d25bd3a889553353eee682f3742c7fc67890d97b5db9a51f89a7091f8'
 
-st.title('Evans Home Weather')
+engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(user, pwd, host, port, db))
+
+df = pd.read_sql_query("select * from weather_station",con=engine)
+
+st.title("Weather Station")
 st.write(df)
