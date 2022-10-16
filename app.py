bcde9107c1c45f565e92766188b854c7b031ae98
Shea2ndemail <50683240+Shea2ndemail@users.noreply.github.com>
2022-10-16 13:09:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ pwd = 'de7a0a0d25bd3a889553353eee682f3742c7fc67890d97b5db9a51f89a7091f8'
 engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(user, pwd, host, port, db))
 
 df = pd.read_sql_query("select * from weather_station",con=engine)
+df = df.iloc[:5]
 
 st.title("Weather Station")
 st.write(df)
