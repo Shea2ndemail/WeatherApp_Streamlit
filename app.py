07f5da907e9708459899ead671179908de20cf68
Shea2ndemail <50683240+Shea2ndemail@users.noreply.github.com>
2022-10-16 12:11:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.DataFrame({'a':[1,2,3,4], 'b': [23423,343243,34234,44444444444]}, index = [0])
+df = pd.DataFrame({'a':[1,2,3,4], 'b': [23423,343243,34234,44444444444]})
 
 st.title('Evans Home Weather')
 st.write(df)
