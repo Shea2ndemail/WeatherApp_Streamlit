e4769086c9b63b91db656b1b01ad3061f34a132a
Shea2ndemail <50683240+Shea2ndemail@users.noreply.github.com>
2022-10-17 07:17:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from sqlalchemy import create_engine
 import pandas as pd
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+
+now = datetime.now()
+cutoff_dt = now - timedelta(hours=24)
 
 host = 'ec2-44-209-24-62.compute-1.amazonaws.com'
 db = 'deqji7h232uoc8'
@@ -11,14 +16,12 @@ pwd = 'de7a0a0d25bd3a889553353eee682f3742c7fc67890d97b5db9a51f89a7091f8'
 engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(user, pwd, host, port, db))
 
 df = pd.read_sql_query("select * from weather_station",con=engine)
-df = df.iloc[-5:]
+df = df[df['datetime']>cutoff_dt]
 
+plt.plot(df['datetime'], df['temperature'])
+st.write(plt)
+
+df = df.iloc[-5:]
 df = df[['datetime', 'ground_temp', 'temperature', 'humidity']]
 
-st.title("Weather Station")
 st.write(df)
-
-if st.button('Say hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
