c5678639acd3da70482bcdae32d6d26aedae97b2
Shea2ndemail <50683240+Shea2ndemail@users.noreply.github.com>
2022-10-17 07:49:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ df = df[df['datetime']>cutoff_dt]
 
 fig, ax = plt.subplots()
 ax.plot(df['datetime'], df['temperature'])
+ax.xaxis.set_major_formatter(dates.DateFormatter('%H:%M'))
 st.pyplot(fig)
 
 df = df.iloc[-5:]
