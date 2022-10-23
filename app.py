1f392176c774e7a4513e2d3663bb9d5976d2e6e0
Shea2ndemail <shea.n.evans@gmail.com>
2022-10-23 10:25:16 -0500

    starting to use plotly

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from sqlalchemy import create_engine
 import pandas as pd
 from datetime import datetime, timedelta
-from matplotlib import pyplot as plt, dates
+import plotly.express as px
 
 now = datetime.now()
 cutoff_dt = now - timedelta(hours=24)
@@ -18,18 +18,10 @@ engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(user, pwd, host, por
 df = pd.read_sql_query("select * from weather_station",con=engine)
 df = df[df['datetime']>cutoff_dt]
 
+fig = px.line( x = df['datetime'] ,
+              y = [df['temperature'], df['ground_temp'], df['humidity']],
+              title = 'Weather')
 
-fig, ax = plt.subplots()
-ax.plot(df['datetime'], df['temperature'])
-plt.tick_params(rotation=45)
-ax.xaxis.set_major_formatter(dates.DateFormatter('%H:%M'))
-st.pyplot(fig)
 
-df = df.iloc[-5:]
-df = df[['datetime', 'ground_temp', 'temperature', 'humidity']]
 
-st.write(df)
-
-test = pd.DataFrame({'a':[2,4,5]})
-
-st.write(test)
+st.plotly_chart(fig, use_container_width=True)
