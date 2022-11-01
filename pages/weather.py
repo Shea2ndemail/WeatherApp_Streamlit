68a83cf850d4d9668cb774a03fb76dd92fd804f0
Shea2ndemail <shea.n.evans@gmail.com>
2022-11-01 12:24:00 -0500

    changing to laptop database

--- pages/weather.py
+++ pages/weather.py
@@ -7,16 +7,10 @@ import plotly.express as px
 now = datetime.now()
 cutoff_dt = now - timedelta(hours=24)
 
-host = 'ec2-44-210-50-83.compute-1.amazonaws.com'
-db = 'd1fp8mklv5sglk'
-user = 'tmrjpxsyzlnbdg'
-port = '5432'
-pwd = 'bf5fa66390bf26d445af2bdfbb4ec899b119150825b6008781acd4abfd267e0b'
-
-engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(user, pwd, host, port, db))
+engine = create_engine('postgresql://postgres:12345@192.168.1.201:5432/test')
 
 df = pd.read_sql_query("select * from weather_station",con=engine)
-df = df[df['datetime']>cutoff_dt]
+df = df.iloc[:15]
 
 fig = px.line( x = df['datetime'] ,
               y = [df['temperature'], df['ground_temp'], df['humidity']],
